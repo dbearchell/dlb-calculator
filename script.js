7b47bff19b36a4342de72eb2ddfb8fdf1eb4a7c1
David Bearchell <dbearchell@gmail.com>
2017-11-09 00:20:30 -0500

    added keys and basic functionality

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.write("Hello World!");
+let activeNumber = 0;
+let calculatorKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "/" , "*", "="]
+for (i = 0; i<calculatorKeys.length; i++){
+  document.write("<button class='calcButton' id=" + calculatorKeys[i] + ">" + calculatorKeys[i] + "</button>")
+}
+document.getElementById("calculatorKeys").innerText = activeNumber;
